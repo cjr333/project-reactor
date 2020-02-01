9568fdb42c0e0d32ad4693bc8e5497688d4db5bf
cjr333 <25564688+cjr333@users.noreply.github.com>
2020-02-01 17:41:58 +0900

    State 기반의 Flux 생성 테스트

---
+++ src/test/java/service/MyService.java
@@ -0,0 +1,87 @@
+package service;
+
+import org.reactivestreams.Subscription;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class MyService {
+  StateOrientedService stateOrientedService = new StateOrientedService();
+
+  public Flux<Chunk> unlimit() {
+    return Flux.interval(Duration.ofMillis(500))
+        .doOnRequest(count -> System.out.println("request of " + count))
+        .doOnNext(idx -> System.out.println("[doOnNext] idx:" + idx))
+        .map(Chunk::getInstance);
+  }
+
+  public Flux<Chunk> limit(int itemCount) {
+    AtomicReference<Subscription> subscriptionRef = new AtomicReference<>();
+    AtomicInteger subscribed = new AtomicInteger();
+    return Flux.interval(Duration.ofMillis(500))
+        .doOnRequest(count -> System.out.println("request of " + count))
+        .doOnSubscribe(subscription -> {
+          subscriptionRef.set(subscription);
+          //                    subscription.request(1);
+        })
+        .map(Chunk::getInstance)
+        .doOnNext(chunk -> {
+          System.out.println("[doOnNext] idx:" + chunk.getLastKey());
+          if (subscribed.addAndGet(chunk.getCount()) >= itemCount) {
+            subscriptionRef.get().cancel();
+          }
+          //                    else {
+          //                        subscriptionRef.get().request(1);
+          //                    }
+        });
+  }
+
+  public Flux<Chunk> limitUsingUnlimit(int itemCount) {
+    AtomicReference<Subscription> subscriptionRef = new AtomicReference<>();
+    AtomicInteger subscribed = new AtomicInteger();
+
+    return unlimit()
+        .doOnSubscribe(subscriptionRef::set)
+        .doOnNext(chunk -> {
+          if (subscribed.addAndGet(chunk.getCount()) >= itemCount) {
+            subscriptionRef.get().cancel();
+          }
+        });
+  }
+
+  //    public Flux<Integer> recursive(int key) {
+  //      final int[] nextKey = {0};
+  //      Flux<Integer> inner = inner(key);
+  //      inner.doOnNext(retVal -> nextKey[0] = retVal);
+  //
+  //      return inner.concatWith(recursive(nextKey[0]));
+  //    }
+  //
+  private Mono<Integer> inner(int key) {
+    return Mono.just(key + 1).delayElement(Duration.ofSeconds(1));
+  }
+
+  public Flux<Integer> generate(int key) {
+    return Flux.generate(
+        () -> key,
+        (state, sink) -> {
+          System.out.println(Thread.currentThread());
+          Integer nextKey = inner(state).block();
+          sink.next(nextKey);
+          //            if (nextKey > 10) {
+          //              sink.complete();
+          //            }
+          return nextKey;
+        });
+  }
+
+  public Flux<Integer> concatMap() {
+    return Flux.range(1, Integer.MAX_VALUE)
+        .doOnRequest(count -> System.out.println("request of " + count))
+        .doOnNext(idx -> System.out.println("[doOnNext] idx:" + idx))
+        .concatMap(i -> stateOrientedService.emit(), 1);
+  }
+}
