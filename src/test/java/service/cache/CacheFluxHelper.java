84959ae7bc10d42f90f02bc35b73624c29be9b7b
cjr333 <25564688+cjr333@users.noreply.github.com>
2020-02-15 22:53:14 +0900

    CacheFluxHelper 작성 및 테스트 작성

--- src/test/java/service/cache/CacheFluxHelper.java
+++ src/test/java/service/cache/CacheFluxHelper.java
@@ -3,25 +3,27 @@ package service.cache;
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
 import lombok.Builder;
-import reactor.cache.CacheMono;
-import reactor.core.publisher.Mono;
-import reactor.core.publisher.Signal;
+import reactor.cache.CacheFlux;
+import reactor.core.publisher.Flux;
 
 import java.time.Duration;
+import java.util.List;
 import java.util.Map;
 import java.util.function.Supplier;
 
 import static org.apache.commons.lang3.ObjectUtils.defaultIfNull;
 
-public class CacheMonoHelper<KEY, VALUE> {
-  private final Supplier<Mono<VALUE>> supplier;
-  private Map<KEY, ? super Signal<? extends VALUE>> cachedEntries;
+public class CacheFluxHelper<KEY, VALUE> {
+  private final Supplier<Flux<VALUE>> supplier;
+  private final Class<VALUE> valueClass;
+  private Map<KEY, ? super List> cachedEntries;
 
   @Builder
-  public CacheMonoHelper(Supplier<Mono<VALUE>> supplier, Duration expire, Integer maxEntry) {
+  public CacheFluxHelper(Supplier<Flux<VALUE>> supplier, Class<VALUE> valueClass, Duration expire, Integer maxEntry) {
     this.supplier = supplier;
+    this.valueClass = valueClass;
 
-    Cache<KEY, ? super Signal<? extends VALUE>> cache = Caffeine
+    Cache<KEY, ? super List> cache = Caffeine
         .newBuilder()
         .expireAfterWrite(defaultIfNull(expire, Duration.ofMinutes(1)))
         .maximumSize(defaultIfNull(maxEntry, 100))
@@ -29,9 +31,9 @@ public class CacheMonoHelper<KEY, VALUE> {
     cachedEntries = cache.asMap();
   }
 
-  public Mono<VALUE> get(KEY key) {
-    return CacheMono
-        .lookup(cachedEntries, key)
+  public Flux<VALUE> get(KEY key) {
+    return CacheFlux
+        .lookup(cachedEntries, key, valueClass)
         .onCacheMissResume(supplier);
   }
 }
