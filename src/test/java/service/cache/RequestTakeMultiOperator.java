c3f7da2133ea969348bb08768b0ac9e8781f273c
cjr333 <25564688+cjr333@users.noreply.github.com>
2020-02-17 15:23:30 +0900

    RequestTakeMultiOperator 작성  - Publisher<T> 에서 Accumulate function 적용  - 종료 조건은 Accumulate 된 A 에 대해서 체크 및 종료 시점에 한번만 onNext 로 accumulated 값 전달

--- src/test/java/service/cache/RequestTakeMultiOperator.java
+++ src/test/java/service/cache/RequestTakeMultiOperator.java
@@ -6,32 +6,45 @@ import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
 
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.BiFunction;
 import java.util.function.Predicate;
 
 @Builder
-public class RequestTakeSingleOperator<T> implements Publisher<T> {
+public class RequestTakeMultiOperator<T, A> implements Publisher<A> {
   private final Publisher<T> source;
   @Builder.Default private final long fetchSize = 5;
-  private Predicate<T> takeWhile;
+  private BiFunction<T, A, A> accumulator;
+  private A initValue;
+  private Predicate<A> takeWhile;
 
   @Override
-  public void subscribe(Subscriber<? super T> s) {
-    source.subscribe(RequestTakeSingleInner.<T>builder().actual(s).fetchSize(fetchSize).takeWhile(takeWhile).build());
+  public void subscribe(Subscriber<? super A> s) {
+    source.subscribe(RequestTakeMultiInner.<T, A>builder()
+        .actual(s)
+        .fetchSize(fetchSize)
+        .accumulator(accumulator)
+        .initValue(initValue)
+        .takeWhile(takeWhile)
+        .build());
   }
 
-  static final class RequestTakeSingleInner<T> implements Subscriber<T>, Subscription {
-    private final Subscriber<? super T> actual;
+  static final class RequestTakeMultiInner<T, A> implements Subscriber<T>, Subscription {
+    private final Subscriber<? super A> actual;
     private final long fetchSize;
-    private Predicate<T> takeWhile;
+    private BiFunction<T, A, A> accumulator;
+    private A accumulated;
+    private Predicate<A> takeWhile;
     private long actualRequest;
     private final AtomicLong request;
 
     private Subscription current;
 
     @Builder
-    RequestTakeSingleInner(Subscriber<? super T> actual, long fetchSize, Predicate<T> takeWhile) {
+    RequestTakeMultiInner(Subscriber<? super A> actual, long fetchSize, BiFunction<T, A, A> accumulator, A initValue, Predicate<A> takeWhile) {
       this.actual = actual;
       this.fetchSize = fetchSize;
+      this.accumulator = accumulator;
+      this.accumulated = initValue;
       this.takeWhile = takeWhile;
       this.request = new AtomicLong(0);
     }
@@ -44,13 +57,14 @@ public class RequestTakeSingleOperator<T> implements Publisher<T> {
 
     @Override
     public void onNext(T t) {
-      if (!takeWhile.test(t)) {
+      accumulated = accumulator.apply(t, accumulated);
+      if (!takeWhile.test(accumulated)) {
+        actual.onNext(accumulated);
         current.cancel();
         onComplete();
         return;
       }
 
-      actual.onNext(t);
       actualRequest--;
       if (this.request.decrementAndGet() == 0) {
         innerRequest(fetchSize);
