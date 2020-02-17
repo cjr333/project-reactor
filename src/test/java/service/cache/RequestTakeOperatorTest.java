e00908db7892ba19cd44e8d1fb821381f96db3f7
cjr333 <cjr333@nexon.co.kr>
2020-02-17 15:23:30 +0900

    RequestTakeMultiOperator 작성  - Publisher<T> 에서 Accumulate function 적용  - 종료 조건은 Accumulate 된 A 에 대해서 체크 및 종료 시점에 한번만 onNext 로 accumulated 값 전달

--- src/test/java/service/cache/RequestTakeOperatorTest.java
+++ src/test/java/service/cache/RequestTakeOperatorTest.java
@@ -9,7 +9,7 @@ import java.time.Duration;
 
 public class RequestTakeOperatorTest {
   @Test
-  public void test() throws InterruptedException {
+  public void testSingle() throws InterruptedException {
     Publisher<Long> push = Flux.interval(Duration.ofMillis(200))
             .doOnRequest(value -> System.out.println("[doOnRequest] " + value))
             .takeWhile(aLong -> aLong < 13)
@@ -32,6 +32,29 @@ public class RequestTakeOperatorTest {
     push_pull.subscribe(new BaseSubscriber<Long>() {
       @Override
       protected void hookOnNext(Long value) {
+        System.out.println("[hookOnNext] " + value);
+      }
+    });
+
+    Thread.sleep(5000);
+  }
+
+  @Test
+  public void testMulti() throws InterruptedException {
+    Publisher<String> push_pull = RequestTakeMultiOperator.<Long, String>builder().source(
+        Flux.interval(Duration.ofMillis(200))
+            .doOnRequest(value -> System.out.println("[doOnRequest] " + value))
+            .doOnNext(aLong -> System.out.println("[doOnNext] " + aLong))
+            //            .take(20)
+            .doOnComplete(() -> System.out.println("[doOnComplete]"))
+    ).accumulator((aLong, s) -> s += aLong)
+        .initValue("")
+        .takeWhile(s -> s.length() < 20)
+        .fetchSize(5).build();
+
+    push_pull.subscribe(new BaseSubscriber<String>() {
+      @Override
+      protected void hookOnNext(String value) {
         System.out.println("[hookOnNext] " + value);
       }
     });
