3d2cf29925d3d3989ef3682735b443fcc5ad6739
cjr333 <cjr333@nexon.co.kr>
2020-02-17 14:15:45 +0900

    RequestOperator 수정  - actual 의 onSubscribe 를 호출하도록 함. 이 때 Subscription 은 현재 RequestOperator 인스턴스로 전달

--- src/test/java/service/cache/RequestOperatorTest.java
+++ src/test/java/service/cache/RequestOperatorTest.java
@@ -2,13 +2,12 @@ package service.cache;
 
 import org.junit.Test;
 import org.reactivestreams.Publisher;
-import org.reactivestreams.Subscriber;
-import org.reactivestreams.Subscription;
+import reactor.core.publisher.BaseSubscriber;
 import reactor.core.publisher.Flux;
 
 import java.time.Duration;
 
-public class TakeOperatorTest {
+public class RequestOperatorTest {
   @Test
   public void test() throws InterruptedException {
     Flux<Long> push = Flux.interval(Duration.ofMillis(200))
@@ -21,35 +20,15 @@ public class TakeOperatorTest {
 
     Thread.sleep(5000);
 
-    Publisher<Long> push_pull = TakeOperator.<Long>builder().source(
+    Publisher<Long> push_pull = RequestOperator.<Long>builder().source(
         Flux.interval(Duration.ofMillis(200))
             .doOnRequest(value -> System.out.println("[doOnRequest] " + value))
             .doOnNext(aLong -> System.out.println("[doOnNext] " + aLong))
             .take(20)
             .doOnComplete(() -> System.out.println("[doOnComplete]"))
-    ).take(5).build();
+    ).fetchSize(5).build();
 
-    push_pull.subscribe(new Subscriber<Long>() {
-      @Override
-      public void onSubscribe(Subscription s) {
-
-      }
-
-      @Override
-      public void onNext(Long aLong) {
-
-      }
-
-      @Override
-      public void onError(Throwable t) {
-
-      }
-
-      @Override
-      public void onComplete() {
-
-      }
-    });
+    push_pull.subscribe(new BaseSubscriber<Long>() {});
 
     Thread.sleep(5000);
   }
