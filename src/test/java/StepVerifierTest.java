87db883114659d09bcddd24eaedcb672ed10964e
cjr333 <25564688+cjr333@users.noreply.github.com>
2020-02-22 23:27:30 +0900

    StepVerifierTest 추가  - recordWith 과 thenConsumeWhile, consumeRecordedWith 의 조합으로 각 아이템의 verify 와 list (sort 같은) 에 대한 verify 가 가능

---
+++ src/test/java/StepVerifierTest.java
@@ -0,0 +1,32 @@
+import org.junit.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+public class StepVerifierTest {
+    @Test
+    public void normal() {
+        StepVerifier.create(Flux.range(0, 10))
+                .expectSubscription()
+                .expectNext(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+                .expectComplete()
+                .verify();
+
+        StepVerifier.create(Flux.range(0, 10))
+                .expectSubscription()
+                .recordWith(ArrayList::new)
+                .thenConsumeWhile(integer -> integer < 10)
+                .consumeRecordedWith(this::verifyList)
+                .expectComplete()
+                .verify();
+    }
+
+    private void verifyList(Collection<Integer> integers) {
+        if (!integers.stream().sorted().collect(Collectors.toList()).equals(integers)) {
+            throw new RuntimeException("sort verification error");
+        }
+    }
+}
