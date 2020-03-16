a1176c8ce059ad98fb54ef21a550051c40bd6189
cjr333 <cjr333@nexon.co.kr>
2020-03-16 18:54:45 +0900

    CaffeineCache 를 직접 사용하는 테스트  - Spring 의 Cacheable annotation 을 사용한 형태 시뮬레이션

---
+++ src/test/java/service/cache/CaffeineCacheTest.java
@@ -0,0 +1,94 @@
+package service.cache;
+
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.junit.Test;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class CaffeineCacheTest {
+  AtomicInteger value = new AtomicInteger(0);
+
+  @Test
+  public void test() throws InterruptedException {
+    Integer key = 1;
+    Cache<Integer, Mono<Long>> cache = Caffeine
+        .newBuilder()
+        .expireAfterWrite(Duration.ofMinutes(1))
+        .maximumSize(100)
+        .build();
+
+    Mono<Long> result = cache.getIfPresent(key);
+    if (result == null) {
+      result = callSimul();
+      cache.put(key, result);
+    }
+    result.subscribe(aLong -> System.out.println("result: " + aLong));
+
+    Thread.sleep(2000);
+
+    result = cache.getIfPresent(key);
+    assert result != null;
+
+    result.subscribe(aLong -> System.out.println("result: " + aLong));
+    Thread.sleep(10000);
+  }
+
+  private Mono<Long> callSimul() {
+    return Mono.just(value.getAndIncrement())
+        .flatMap(this::innerFlatMap);
+  }
+
+  private Mono<Long> innerFlatMap(int integer) {
+    return Mono.just(System.currentTimeMillis() * 100 + integer).delayElement(Duration.ofSeconds(2));
+  }
+
+  @Test
+  public void test2() throws InterruptedException {
+    Integer key = 1;
+    Cache<Integer, Mono<List<String>>> cache = Caffeine
+        .newBuilder()
+        .expireAfterWrite(Duration.ofMinutes(1))
+        .maximumSize(100)
+        .build();
+
+    Mono<List<String>> result = cache.getIfPresent(key);
+    if (result == null) {
+      result = isSvcIdAvailable("2");
+      cache.put(key, result);
+    }
+    result.subscribe(aLong -> System.out.println("result: " + aLong));
+
+    Thread.sleep(2000);
+
+    result = cache.getIfPresent(key);
+    assert result != null;
+
+    result.subscribe(aLong -> System.out.println("result: " + aLong));
+    Thread.sleep(10000);
+  }
+
+  public Flux<String> generateSampleList() {
+    System.out.println("generateSampleList");
+    return Flux.range(0, 3).map(String::valueOf)
+        .onErrorResume(e -> Mono.error(new IllegalArgumentException()));
+  }
+
+  public Mono<List<String>> isSvcIdAvailable(String input) {
+    return generateSampleList()
+        .flatMap(sample -> {
+          if (sample.equals(input)){
+            return generateSampleList();
+          } else {
+            return Flux.empty();
+          }
+        })
+        .switchIfEmpty(Mono.error(new IllegalArgumentException()))
+        .collectList()
+        ;
+  }
+}
