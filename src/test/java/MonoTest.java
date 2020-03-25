100deda451a45b4957c51741960ea555c401f5c2
cjr333 <cjr333@nexon.co.kr>
2020-03-26 01:03:52 +0900

    retry delay 테스트

---
+++ src/test/java/MonoTest.java
@@ -0,0 +1,46 @@
+import org.junit.Test;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+
+public class MonoTest {
+  @Test
+  public void emtpyTest() throws InterruptedException {
+    composeLog(Mono.empty())
+        .subscribe(integer -> System.out.println("[Subscriber] " + integer));
+
+    composeLog(Mono.just(1))
+        .subscribe(integer -> System.out.println("[Subscriber] " + integer));
+    Thread.sleep(10000);
+  }
+
+  private <T> Mono<T> composeLog(Mono<T> actual) {
+    return actual.doOnNext(t -> System.out.println("[doOnNext] " + t))
+        .doOnSuccess(t -> System.out.println("[doOnSuccess] " + t))
+        .doOnTerminate(() -> System.out.println("[doOnTerminate]"));
+  }
+
+  @Test
+  public void retryTest() throws InterruptedException {
+    System.out.println("No delay");
+    Mono.error(new NullPointerException())
+        .doOnError(throwable -> System.out.println(System.currentTimeMillis()))
+        .retry(3)
+        .subscribe();
+    Thread.sleep(2000);
+
+    System.out.println("Constant delay");
+    Mono.error(new NullPointerException())
+        .doOnError(throwable -> System.out.println(System.currentTimeMillis()))
+        .retryBackoff(3, Duration.ofSeconds(1), Duration.ofSeconds(1))
+        .subscribe(null, System.out::println);
+    Thread.sleep(5000);
+
+    System.out.println("Backoff delay");
+    Mono.error(new NullPointerException())
+        .doOnError(throwable -> System.out.println(System.currentTimeMillis()))
+        .retryBackoff(3, Duration.ofMillis(234))
+        .subscribe(null, System.out::println);
+    Thread.sleep(5000);
+  }
+}
